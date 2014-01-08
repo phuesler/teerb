82def230e148e1a25b4c88aa10b016a0157bdbbe
Patrick Huesler <patrick.huesler@gmail.com>
2014-01-08 16:23:52 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# TeeRb - Several ways to capture and tee Ruby's standard input and output
+
+  * Pipe calls to $stdout and $stderr to a file
+  * Pipe calls to $stdout and $stderr to an instance of Ruby's logger class
+  * Pipe calls to an instance of Ruby's logger class to $stdout
+
+## Usage
+
+
+    # Tee $stdout and $stderr to several files
+    log_file = File.open("debug.log", "a")
+    tee = TeeRb::Tee.new(log_file)
+    tee.enable
+
+    puts 'puts hello'
+    $stdout.puts 'stdout hello'
+    $stderr.puts 'stderr hello'
+
+    tee.disable
+
+    puts 'puts hello'
+    $stdout.puts 'stdout hello'
+    $stderr.puts 'stderr hello'
+
+    # same as above but with block syntax
+    log_file = File.open("debug.log", "a")
+    TeeRb::Tee.new(log_file) do
+      puts 'puts hello'
+      $stdout.puts 'stdout hello'
+      $stderr.puts 'stderr hello'
+    end
+
+
+    # Capture $stdout and $stderr and send it through `logger`
+    require 'logger'
+    log_file = File.open("debug.log", "a")
+    logger = Logger.new(log_file)
+    tee = TeeRb::LoggerTee.new(logger)
+    tee.enable
+
+    logger.warn "hello"
+    $stderr.puts "stderr hello"
+    puts "stdout hello"
+
+
+  * Pipe calls to an instance of Ruby's logger class to $stdout
+    require 'logger'
+    log_file = File.open("debug.log", "a")
+    logger = Logger.new(TeeRb::IODelegate.new(log_file, STDOUT))
+    logger.warn "warn"
+    $stderr.puts "stderr hello"
+    puts "stdout hello"
+
+## Problems
+
+This only works for `$stdout` and `$stderr` and not for `STDOUT` and `STDERR`, since are constants
+that should not be reassigned.
---
+++ teerb.rb
@@ -0,0 +1,115 @@
+require 'logger'
+
+module TeeRb
+  class IODelegate
+    def initialize(*targets)
+       @targets = targets
+    end
+
+    def write(*args)
+      @targets.each {|t| t.write(*args)}
+    end
+
+    def puts(*args)
+      write(*args)
+      write("\n")
+    end
+
+    def put(*args)
+      write(*args)
+    end
+
+    def close
+      @targets.each(&:close)
+    end
+  end
+
+  class LoggerIODelegate
+    def initialize(logger, log_level)
+       @logger    = logger
+       @log_level = log_level
+    end
+
+    def write(*args)
+      @logger.send(@log_level, *args) unless args == ["\n"]
+    end
+
+    def puts(*args)
+      write(*args)
+    end
+
+    def put(*args)
+      write(*args)
+    end
+
+    def close
+      @logger.close
+    end
+  end
+
+
+  class Tee
+    def initialize(*logfiles, &block)
+      @stdout_tee      = IODelegate.new(*([STDOUT] + logfiles))
+      @stderr_tee      = IODelegate.new(*([STDERR] + logfiles))
+      @original_stdout = $stdout
+      @original_stderr = $stderr
+
+      if block_given?
+        enable
+        yield
+        disable
+      end
+    end
+
+    def enable
+      $stdout = @stdout_tee
+      $stderr = @stderr_tee
+    end
+
+    def disable
+      $stdout = @original_stdout
+      $stderr = @original_stderr
+    end
+  end
+
+  #sends stdout to logger.info
+  #sends stderror to logger.error
+  #sends everything to STDOUT
+  class LoggerTee
+    def initialize(logger)
+      @stdout_tee      =  IODelegate.new(LoggerIODelegate.new(logger, :info) , STDOUT)
+      @stderr_tee      =  IODelegate.new(LoggerIODelegate.new(logger, :error), STDERR)
+
+      @original_stdout = $stdout
+      @original_stderr = $stderr
+
+      if block_given?
+        enable
+        yield
+        disable
+      end
+    end
+
+    def enable
+      $stdout = @stdout_tee
+      $stderr = @stderr_tee
+    end
+
+    def disable
+      $stdout = @original_stdout
+      $stderr = @original_stderr
+    end
+  end
+end
+
+
+    require 'logger'
+
+    log_file = File.open("debug.log", "a")
+    logger = Logger.new(TeeRb::IODelegate.new(log_file, STDOUT))
+    tee = TeeRb::LoggerTee.new(logger)
+    tee.enable
+    logger.warn "warn"
+    $stderr.puts "stderr hello"
+    puts "stdout hello"
